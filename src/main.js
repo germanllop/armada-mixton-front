6148b42534559ab412ad9fbb64f11ed35c548817
German Llop <germanllop@gmail.com>
2020-02-27 03:46:49 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/scss/main.scss'
+import '@/assets/js/helpers'
+
 Vue.config.productionTip = false
 
 new Vue({
