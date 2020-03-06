6ad2460aa054b72986731ae465399782051c8918
German Llop <germanllop@gmail.com>
2020-03-06 03:31:28 -0500

    Some links

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/auth/login">Login</router-link> |
+      <router-link to="/auth/steam">Login</router-link> |
       <router-link to="/auth/logout">Logout</router-link> |
       <router-link to="/api/">CheckAPI</router-link>
     </div>
