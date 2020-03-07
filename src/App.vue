c6a13fc474ba7937f275e48c2ba5fb9f51b3e46c
unknown <germanllop@gmail.com>
2020-03-06 20:19:46 -0500

    API connection to store user

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app" class="">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/auth/steam">Login</router-link> |
-      <router-link to="/auth/logout">Logout</router-link> |
-      <router-link to="/api/">CheckAPI</router-link>
+      <router-link to="/player">Player</router-link> |
+      <a href="/auth/steam">Login</a> |
+      <a href="/auth/logout">Logout</a> |
+      <a href="/api/">CheckAPI</a>
     </div>
     <router-view/>
   </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    me:{},
+    friends:[],
+    players:[]
   },
   mutations: {
+    setCurrentPlayer: (state, player) => (state.me = player),
+    fillPlayers: (state, players) => (state.players = players),
+    fillFriends: (state, friends) => (state.friends = friends)
   },
   actions: {
+    async fetchMe({ commit }) {
+      const res = await axios.get('api/getPlayer')
+      //console.log(res)
+      commit('setCurrentPlayer', res.data)
+    },
+    async fetchPlayers({ commit }) {
+        const res = await axios.get('api/getPlayers')
+        commit('fillPlayers', res.data)
+    },
+    async fetchFriends({ commit }) {
+        const res = await axios.get('api/getFriends')
+        commit('fillFriends', res.data)
+    }
   },
   modules: {
   }
