5c379bf2e4738f8f9209e880e48f29f500dee10d
German Llop <germanllop@gmail.com>
2020-03-10 03:01:03 -0500

    Images added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <h1>Main Login window</h1>
+    <div class="flex flex-wrap h-screen">
+        <div class="w-1/2 bg-local bg-center bg-cover" style="background-image: url('./images/armada-cover.jpg');"></div>
+        <div class="w-1/2 flex text-center justify-center content-center flex-col flex-wrap">
+            <a href="/auth/steam"><img src="https://steamcommunity-a.akamaihd.net/public/shared/images/signinthroughsteam/sits_landing.png" alt=""></a>
+        </div>
     </div>
 </template>
 <script>
