453e9f802b8124314cbd6b8b61df9ae340d4ec1c
unknown <germanllop@gmail.com>
2020-03-09 21:49:51 -0500

    Store with check auth

--- src/store/index.js
+++ src/store/index.js
@@ -11,23 +11,23 @@ export default new Vuex.Store({
     players:[]
   },
   mutations: {
-    setCurrentPlayer: (state, player) => (state.me = player),
-    fillPlayers: (state, players) => (state.players = players),
-    fillFriends: (state, friends) => (state.friends = friends)
+    SET_CURRENT_PLAYER: (state, player) => (state.me = player),
+    SET_PLAYERS: (state, players) => (state.players = players),
+    SET_FRIENDS: (state, friends) => (state.friends = friends)
   },
   actions: {
-    async fetchMe({ commit }) {
-      const res = await axios.get('api/getPlayer')
-      //console.log(res)
-      commit('setCurrentPlayer', res.data)
+    async checkAuth({ commit }) {
+      const res = await axios.get('auth/check')
+      console.log(res)
+      commit('SET_CURRENT_PLAYER', res.data)
     },
     async fetchPlayers({ commit }) {
         const res = await axios.get('api/getPlayers')
-        commit('fillPlayers', res.data)
+        commit('SET_PLAYERS', res.data)
     },
     async fetchFriends({ commit }) {
         const res = await axios.get('api/getFriends')
-        commit('fillFriends', res.data)
+        commit('SET_FRIENDS', res.data)
     }
   },
   modules: {
