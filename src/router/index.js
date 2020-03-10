b294f3511285fc8e6d1134fb6402e6e44c805889
German Llop <germanllop@gmail.com>
2020-03-10 03:00:44 -0500

    Completed base routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Lobby from '../views/Lobby.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/lobby',
+    name: 'Lobby',
+    component: Lobby
+  },
+  {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/player',
-    name: 'Player',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Player.vue')
   }
 ]
 
