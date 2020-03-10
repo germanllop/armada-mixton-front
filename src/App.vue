b81e2012c514dc5a138f3f678af83d80b79a48ab
unknown <germanllop@gmail.com>
2020-03-09 21:50:08 -0500

    Updated views for new schema

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <div id="app" class="">
-    <div id="nav">
+  <div id="app">
+    <!-- <div>
       <router-link to="/">Home</router-link> |
       <router-link to="/player">Player</router-link> |
       <a href="/auth/steam">Login</a> |
       <a href="/auth/logout">Logout</a> |
       <a href="/api/">CheckAPI</a>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-
-</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Main Login window</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Login',
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
