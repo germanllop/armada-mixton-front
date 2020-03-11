06d5c44b89ae3cff4be22a5d429a52081ab92cb0
German Llop <germanllop@gmail.com>
2020-03-11 04:08:43 -0500

    404 page

---
+++ src/views/404.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex flex-wrap h-screen">
+        <div class="w-1/2 bg-local bg-center bg-cover" style="background-image: url('./images/armada-cover.jpg');"></div>
+        <div class="w-1/2 flex text-center justify-center content-center flex-col flex-wrap">
+            <img src="/images/not_found.gif" alt="" class="w-1/2 my-3">
+            <p class="my-3"><router-link class="bg-green-600 hover:bg-green-500 text-white py-2 px-4" to="/">Regresar</router-link></p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: '404',
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+    },
+    methods:{
+
+    }
+}
+</script>
