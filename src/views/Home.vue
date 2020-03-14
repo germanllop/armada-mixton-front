3647a1bbb8ec150e6e1c257535860879e7ef83b5
unknown <germanllop@gmail.com>
2020-03-13 21:18:55 -0500

    Navbar complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex text-white">
+  <div class="flex text-white font-sans">
     <div class="flex flex-col w-full">
-      <nav class="flex items-center justify-between flex-wrap p-6">
-        <div class="flex items-center flex-shrink-0 mr-6">
+      <nav class="flex items-center justify-between flex-wrap p-5">
+        <div class="flex items-center flex-shrink-0 mx-5">
           <img src="images/logo.png" alt="Armada Offensive" width="200px">
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="lg:flex-grow">
-            <router-link class="block mt-4 inline-block text-xl font-medium hover:text-white mr-4" to="/">Home</router-link>
-            <router-link class="block mt-4 inline-block text-xl font-medium hover:text-white mr-4" to="/lobby">Lobby</router-link>
+            <router-link active-class="nav-link-active" class="nav-link" to="/">Mixtones</router-link>
+            <router-link active-class="nav-link-active" class="nav-link" to="/lobby">Mi Lobby</router-link>
 
           </div>
           
